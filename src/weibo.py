c7e84b0c78d4db1d8fe94bbf979d5579732e1048
zhangnd <920563959@qq.com>
2024-03-29 18:02:01 +0800

    update weibo.py

--- src/weibo.py
+++ src/weibo.py
@@ -1,4 +1,7 @@
+import json
+
 import requests
+from openpyxl import Workbook
 from requests import HTTPError
 
 
@@ -22,18 +25,57 @@ def get_html(url):
 
 def get_info(uid):
     url = 'https://weibo.com/ajax/profile/info?uid=%d' % uid
-    content = get_html(url)
+    html = get_html(url)
+    data = json.loads(html)
+    ok = data['ok']
+    if ok == 1:
+        user = data['data']['user']
+        return user
 
 
 def get_detail(uid):
     url = 'https://weibo.com/ajax/profile/detail?uid=%d' % uid
-    content = get_html(url)
+    html = get_html(url)
+    data = json.loads(html)
+    ok = data['ok']
+    if ok == 1:
+        data = data['data']
+        return data
+
+
+def get_data(uid):
+    info = get_info(uid)
+    detail = get_detail(uid)
+    if info and detail:
+        data = {**info, **detail}
+        return data
 
 
 def main():
     uid = 1669879400
-    get_info(uid)
-    get_detail(uid)
+    workbook = Workbook()
+    sheet = workbook.active
+    sheet['A1'] = 'id'
+    sheet['B1'] = '昵称'
+    sheet['C1'] = '微博认证'
+    sheet['D1'] = '简介'
+    sheet['E1'] = '生日'
+    sheet['F1'] = '所在地'
+    sheet['G1'] = '学校'
+    sheet['H1'] = '公司'
+    data = get_data(uid)
+    if data:
+        sheet.append([
+            str(data['id']),
+            data['screen_name'],
+            data['desc_text'],
+            data['description'],
+            data['birthday'],
+            data['location'],
+            data.get('education', {}).get('school'),
+            data.get('company')
+        ])
+    workbook.save('微博.xlsx')
 
 
 if __name__ == '__main__':
