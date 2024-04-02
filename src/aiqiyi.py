a5b8de1bf6bbccb545879fc37ef330194946a1e0
zhangnd <920563959@qq.com>
2024-04-02 16:16:00 +0800

    update aiqiyi.py

--- src/aiqiyi.py
+++ src/aiqiyi.py
@@ -53,11 +53,9 @@ def get_base_info(tvid):
 
 
 def get_m3u8(html, tvid):
-    pattern = re.compile('ptid=(.*?)&')
-    result = re.search(pattern, html)
-    bid = 600
-    vid = '74367c03493325bc536d782ff3eafd30'
-    src = result.group(1).strip() if result else ''
+    bid = 0
+    vid = ''
+    src = ''
     k_uid = ''
     authKey = ''
     dfp = ''
@@ -65,8 +63,9 @@ def get_m3u8(html, tvid):
     url = 'https://cache.video.iqiyi.com/dash?tvid=%d&bid=%d&vid=%s&src=%s&vt=0&rs=1&uid=&ori=pcw&ps=1&k_uid=%s&pt=0&d=0&s=&lid=0&cf=0&ct=0&authKey=%s&k_tag=1&dfp=%s&locale=zh_cn&pck=&k_err_retries=0&up=&sr=1&qd_v=5&tm=%d&qdy=u&qds=0' % (
         tvid, bid, vid, src, k_uid, authKey, dfp, tm
     )
-    # url = 'https://cache.video.iqiyi.com/dash?tvid=244044600&bid=600&vid=74367c03493325bc536d782ff3eafd30&src=01010031010000000000&vt=0&rs=1&uid=&ori=pcw&ps=1&k_uid=6c09fed07aaf6079a2fb60acd9e4c80e&pt=0&d=0&s=&lid=0&cf=0&ct=0&authKey=e1977be072c1b12594238952d21351f7&k_tag=1&dfp=a09a3f6da97bf74c09a9e24aec21fa35114fc66b8646e7f364e12e87a736f905df&locale=zh_cn&pck=&k_err_retries=0&up=&sr=1&qd_v=5&tm=1711934900812&qdy=u&qds=0&k_ft1=706436220846084&k_ft4=1162321298202628&k_ft2=262335&k_ft5=134217729&k_ft6=128&k_ft7=688390148&fr_300=120_120_120_120_120_120&fr_500=120_120_120_120_120_120&fr_600=120_120_120_120_120_120&fr_800=120_120_120_120_120_120&fr_1020=120_120_120_120_120_120&bop=%7B%22version%22%3A%2210.0%22%2C%22dfp%22%3A%22a09a3f6da97bf74c09a9e24aec21fa35114fc66b8646e7f364e12e87a736f905df%22%2C%22b_ft1%22%3A24%7D&ut=0&vf=ee904be1fd0de13d0fb21d06af12d9e6'
+    url = 'https://cache.video.iqiyi.com/dash?tvid=244044600&bid=600&vid=74367c03493325bc536d782ff3eafd30&src=01010031010000000000&vt=0&rs=1&uid=&ori=pcw&ps=1&k_uid=6c09fed07aaf6079a2fb60acd9e4c80e&pt=0&d=0&s=&lid=0&cf=0&ct=0&authKey=e1977be072c1b12594238952d21351f7&k_tag=1&dfp=a09a3f6da97bf74c09a9e24aec21fa35114fc66b8646e7f364e12e87a736f905df&locale=zh_cn&pck=&k_err_retries=0&up=&sr=1&qd_v=5&tm=1711934900812&qdy=u&qds=0&k_ft1=706436220846084&k_ft4=1162321298202628&k_ft2=262335&k_ft5=134217729&k_ft6=128&k_ft7=688390148&fr_300=120_120_120_120_120_120&fr_500=120_120_120_120_120_120&fr_600=120_120_120_120_120_120&fr_800=120_120_120_120_120_120&fr_1020=120_120_120_120_120_120&bop=%7B%22version%22%3A%2210.0%22%2C%22dfp%22%3A%22a09a3f6da97bf74c09a9e24aec21fa35114fc66b8646e7f364e12e87a736f905df%22%2C%22b_ft1%22%3A24%7D&ut=0&vf=ee904be1fd0de13d0fb21d06af12d9e6'
     url = 'https://cache.video.iqiyi.com/dash?tvid=244044600&bid=600&vid=74367c03493325bc536d782ff3eafd30&src=01010031010000000000&vt=0&rs=1&uid=&ori=pcw&ps=1&k_uid=3c54dtcsl4hpjj2pojutj5362m2m454j&pt=0&d=0&s=&lid=0&cf=0&ct=0&authKey=535d06fd2856b1e9e8bf8a15b30e2c36&k_tag=1&dfp=a0e901cef978c8495f8b06273621fd24b50ec704be660c775a039f3192640e4f6e&locale=zh_cn&pck=&k_err_retries=0&up=&sr=1&qd_v=5&tm=1711972450835&qdy=u&qds=0&k_ft1=706436220846084&k_ft4=1162321298202628&k_ft2=262335&k_ft5=134217729&k_ft6=128&k_ft7=688390148&fr_300=120_120_120_120_120_120&fr_500=120_120_120_120_120_120&fr_600=120_120_120_120_120_120&fr_800=120_120_120_120_120_120&fr_1020=120_120_120_120_120_120&bop=%7B%22version%22%3A%2210.0%22%2C%22dfp%22%3A%22a0e901cef978c8495f8b06273621fd24b50ec704be660c775a039f3192640e4f6e%22%2C%22b_ft1%22%3A24%7D&ut=0&vf=a1bb8a7b6ae5c0b071560bc8b9daf79e'
+    url = 'https://cache.video.iqiyi.com/dash?tvid=244044600&bid=300&vid=64e1e65203acb7d8c7a3182a3ada45e1&src=01010031010000000000&vt=0&rs=1&uid=&ori=pcw&ps=1&k_uid=39b201a4226cf0786ad119a1601e9632&pt=0&d=0&s=&lid=0&cf=0&ct=0&authKey=43807d7bb2f557edf0ba08d377add5e9&k_tag=1&dfp=a09a3f6da97bf74c09a9e24aec21fa35114fc66b8646e7f364e12e87a736f905df&locale=zh_cn&pck=&k_err_retries=0&up=&sr=1&qd_v=5&tm=1712043202930&qdy=u&qds=0&k_ft1=706436220846084&k_ft4=1162321298202628&k_ft2=262335&k_ft5=134217729&k_ft6=128&k_ft7=688390148&fr_300=120_120_120_120_120_120&fr_500=120_120_120_120_120_120&fr_600=120_120_120_120_120_120&fr_800=120_120_120_120_120_120&fr_1020=120_120_120_120_120_120&bop=%7B%22version%22%3A%2210.0%22%2C%22dfp%22%3A%22a09a3f6da97bf74c09a9e24aec21fa35114fc66b8646e7f364e12e87a736f905df%22%2C%22b_ft1%22%3A24%7D&ut=0&vf=2057da5a6a568ee203b63cc2a9408c7f'
     html = get_html(url)
     if html:
         data = json.loads(html)
